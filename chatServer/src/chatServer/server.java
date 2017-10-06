987c15b32429ad2a1a3e2c8b0d7eaef96270237d
Callum Duffy <duffyc8@tcd.ie>
2017-10-06 12:36:33 +0100

    initial simple server class added

---
+++ chatServer/src/chatServer/server.java
@@ -0,0 +1,22 @@
+package chatServer;
+
+import java.io.*;
+import java.net.*;
+class chatProtocol{
+	public static void main(String argv[]) throws Exception {
+		
+		String clientSentence, capitalisedSentence;
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+		
+			while(true) {
+				Socket conSocket = welcomeSocket.accept();
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(
+						conSocket.getInputStream()));
+				DataOutputStream outToClient = new
+						DataOutputStream(conSocket.getOutputStream());
+						clientSentence = inFromClient.readLine();
+						capitalisedSentence = clientSentence.toUpperCase() + "\n";
+						outToClient.writeBytes(capitalisedSentence);
+		}
+	}				
+}
