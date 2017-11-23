8b89fe59e68bb13e7b51f0e82f5dc1f40ab845b7
duffyc8 <duffyc8@tcd.ie>
2017-11-23 18:44:31 +0000

    fixed final exceptions.

--- chatProtocol/src/chatProtocol/ServerThread.java
+++ chatProtocol/src/chatProtocol/ServerThread.java
@@ -235,7 +235,10 @@ public class ServerThread extends Thread {
     	}
     		socket.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+        }
+        catch(NullPointerException e){
+        	
         }
 	}
     public static int typeCheck(String input){
